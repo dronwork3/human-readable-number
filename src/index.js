276c9ad579d57261a96d46dc30f8587b40f5eafb
Andrey Novikov <dronwork3@gmail.com>
2020-03-21 22:06:09 +0300

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const arrNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+
+    const arrTenths = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+        'eighty', 'ninety']
+
+    if (number < 20) {
+        return arrNumbers[number];
+    }
+
+    if (number >= 20 && number < 100) {
+        if (number % 10 === 0) {
+            return arrTenths[number / 10];
+        } else {
+            return arrTenths[Math.floor(number / 10)] + ' ' + arrNumbers[number % 10];
+        }
+    }
+
+    if (number > 99) {
+        if (number % 100 === 0) {
+            return arrNumbers[number / 100] + ' ' + 'hundred';
+        } else if (number % 10 === 0) {
+            return arrNumbers[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + arrTenths[(Math.floor(number / 10)) % 10];
+        }
+        else if (number % 100 < 20) {
+            return arrNumbers[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + arrNumbers[number % 100];
+        } else {
+            return arrNumbers[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
+                arrTenths[(Math.floor(number / 10)) % 10] + ' ' + arrNumbers[number % 10]
+        }
+
+    }
 }
